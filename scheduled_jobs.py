eafa404311cdc011015b4a04de1d064579a948cf
AndyW-2025 <andrew.walton@capita.com>
2026-07-14 11:22:41 +0000

    back to orig logging

--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -34,7 +34,7 @@ def process_orders(app):
             json=payload
         )
 
-        app.logger.info("Response from endpoint: " +  payload)
+        app.logger.info("Response from endpoint: " + response.text)
         response.raise_for_status()
 
         order.set_as_processed()
